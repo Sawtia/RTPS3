d3d86e0809aeb036755c9b9486ebcc01493d5da5
Sergey <ghostgm@yandex.ru>
2022-05-08 18:52:37 +0300

    Version 2

--- main.py
+++ main.py
@@ -17,53 +17,53 @@
 # print(c)
 # # print(list(test.values())[2])
 #
-def logpas():
-    login = input('login:')
-    password = input('password:')
-    return login,password
-
-def login():
-    login,password= logpas()
-    if login in users.keys():
-        if users[login] == password:
-            print('ok')
-            print(secret)
-
-def registration():
-    login,password= logpas()
-    if login in users.keys():
-        print('busy')
-    else:
-        users[login] = password
-        print('susses')
-
-def delete():
-    delete = input('введите:')
-    if delete in users.keys():
-        password = input('password:')
-        if users[delete] == password:
-            del users[delete]
-            print('ok')
-
-def error():
-    print('mistake')
-
-
-
-users={'admin':'123'}
-secret='42'
-while True:
-    q1 = input('Вход или регистрация или удаление?')
-    if q1 == 'Вход':
-        login()
-        break
-    elif q1 == 'Регистрация':
-        registration()
-    elif q1 == 'удаление':
-        delete()
-    else:
-        error()
-
+# def logpas():
+#     login = input('login:')
+#     password = input('password:')
+#     return login,password
+#
+# def login():
+#     login,password= logpas()
+#     if login in users.keys():
+#         if users[login] == password:
+#             print('ok')
+#             print(secret)
+#
+# def registration():
+#     login,password= logpas()
+#     if login in users.keys():
+#         print('busy')
+#     else:
+#         users[login] = password
+#         print('susses')
+#
+# def delete():
+#     delete = input('введите:')
+#     if delete in users.keys():
+#         password = input('password:')
+#         if users[delete] == password:
+#             del users[delete]
+#             print('ok')
+#
+# def error():
+#     print('mistake')
+#
+#
+#
+# users={'admin':'123'}
+# secret='42'
+# while True:
+#     q1 = input('Вход или регистрация или удаление?')
+#     if q1 == 'Вход':
+#         login()
+#         break
+#     elif q1 == 'Регистрация':
+#         registration()
+#     elif q1 == 'удаление':
+#         delete()
+#     else:
+#         error()
+#
 
 
 # def summ_two_numb(num1,num2):
@@ -72,5 +72,31 @@ while True:
 # summ=summ_two_numb(1,2)
 # print(summ)
 #
+
+
+menu = {'pizza1':'100',
+        'pizza2':'200'}
+
+def add_pizza(name,price):
+    if name in menu.keys():
+        print('already there is')
+    else:
+        menu[name] = price
+print('before')
+print(menu)
+add_pizza('pizza3',100)
+print('after')
+print(menu)
+
+
+def remove_pizza(name):
+    if name in menu.keys()
+        print('delete')
+        del menu[name]
+    else:
+        print('no pizza')
+
+
+def order
 
 
