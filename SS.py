25ea86393cc37a12c23ec25492bdaf6a4e9412a9
Sergey <ghostgm@yandex.ru>
2022-05-08 18:26:29 +0300

    Version 1

--- SS.py
+++ SS.py
@@ -1,1 +1,6 @@
-print(100)
+tuple=(1,2,3,4)
+a = {1,2,3,4}
+list=[1,2,3,4]
+
+b=set(list)
+print(type(b))
--- main.py
+++ main.py
@@ -1,16 +1,76 @@
-# This is a sample Python script.
+# tuple=(1,2,3,4)
+# a = {1,2,3,4}
+# list=[1,2,3,4]
+#
+# b=set(list)
+# print(type(b))
+#
+# test = {'login':'123',
+#         'ivan': '143',
+#         'max': '67'}
+# # print(test['login'])
+# # print(test.keys())
+# # print(test.values())
+# a=test.keys()
+# b = list(a)
+# c=b[0]
+# print(c)
+# # print(list(test.values())[2])
+#
+def logpas():
+    login = input('login:')
+    password = input('password:')
+    return login,password
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def login():
+    login,password= logpas()
+    if login in users.keys():
+        if users[login] == password:
+            print('ok')
+            print(secret)
+
+def registration():
+    login,password= logpas()
+    if login in users.keys():
+        print('busy')
+    else:
+        users[login] = password
+        print('susses')
+
+def delete():
+    delete = input('введите:')
+    if delete in users.keys():
+        password = input('password:')
+        if users[delete] == password:
+            del users[delete]
+            print('ok')
+
+def error():
+    print('mistake')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+users={'admin':'123'}
+secret='42'
+while True:
+    q1 = input('Вход или регистрация или удаление?')
+    if q1 == 'Вход':
+        login()
+        break
+    elif q1 == 'Регистрация':
+        registration()
+    elif q1 == 'удаление':
+        delete()
+    else:
+        error()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# def summ_two_numb(num1,num2):
+#     summ=num1+num2
+#     return summ
+# summ=summ_two_numb(1,2)
+# print(summ)
+#
+
+
